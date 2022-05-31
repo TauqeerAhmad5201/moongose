c10b5c8e2e716c8664c096be2a0a648c1219835f
Tauqeer Ahmad <hellotauqeer@gmail.com>
2022-05-31 16:26:19 +0530

    added code for express form

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const mongoose = require('mongoose');
-
+var express = require('express')
+var bodyParser = require('body-parser')
+const port = 3000
 main().catch(err => console.log(err));
+
+var app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/sign', (req, res) => {
+  res.sendFile(__dirname + '/public/html/signin.html')
+})
 
 async function main() {
   await mongoose.connect('mongodb+srv://tauqeerahmad:mantasha786@practicecluster.kqrhg.mongodb.net/?retryWrites=true&w=majority');
 
   console.log('We are connected!');
   const kittySchema = new mongoose.Schema({
-    name: String
+    name: {
+      first : String,
+      last : String 
+    }
   });
-
+  console.log(kittySchema.path('name'))
   kittySchema.methods.speak = function speak() {
     const greeting = this.name
       ? "Meow name is " + this.name
@@ -17,12 +35,23 @@ async function main() {
     console.log(greeting);
   };
   const Kitten = mongoose.model('Kitten', kittySchema); //model
-
-  const silence = new Kitten({ name: 'Silence' });
-  console.log(silence.name); //
-  silence.speak();
-  silence.save();
+  // mongoose.model(modelName, schema):
+  app.post('/sign', (req, res) => {
+    const silence = new Kitten({name : { first: req.body.name, last : req.body.password }});
+    console.log(silence.name); //
+    silence.save();
+    res.send('Thanks for submitting')
+  })
+  
+  // silence.speak();
+  
 
   const kittens = await Kitten.find();
-  console.log(kittens);
+  // console.log(kittens);
+  const datafind = await Kitten.find({ _id : "62941771affe10bf448ce739"});
+  console.log(datafind)
 }
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
